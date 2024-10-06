95521962d9a38c3cdc54a8b68efc77a0392c7668
Sirvan Monfared <sirvan.monfared.dev@gmail.com>
2024-10-06 05:36:21 +0330

    s13e01

--- resources/views/components/input.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="col-md-6 mb-3">
-    <div class="form-group has-error">
-        <label for="">{{ $label }}</label>
-        <input type="{{ $type ?? 'text' }}" name="{{ $name }}" @class(['form-control', 'is-invalid' => $errors->has($name)])  value="{{ old($name, $value ?? null) }}">
-        @error($name)<p class="text-danger">{{ $message }}</p>@enderror
-    </div>
-</div>
